199cc8f5b0b7cc3102f9e43b3bca7e329ed19a71
Naomi Schettini <>
2019-02-22 23:07:03 -0500

    Organized project structure and added code for API manager call & Media type structures

---
+++ iTunesMedia/APIManager/APIManager.swift
@@ -0,0 +1,9 @@
+//
+//  APIManager.swift
+//  iTunesMedia
+//
+//  Created by Naomi Schettini on 2/22/19.
+//  Copyright © 2019 Naomi Schettini. All rights reserved.
+//
+
+import Foundation
---
+++ iTunesMedia/Model/Media.swift
@@ -0,0 +1,9 @@
+//
+//  Media.swift
+//  iTunesMedia
+//
+//  Created by Naomi Schettini on 2/22/19.
+//  Copyright © 2019 Naomi Schettini. All rights reserved.
+//
+
+import Foundation
